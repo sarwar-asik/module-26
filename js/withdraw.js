e7a7dc01bc1e4a9273a03e274959a4b0bfc86157
sarwar-asik <shasik2021@gmail.com>
2022-08-16 04:22:00 +0600

    bank website

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,15 +3,21 @@ document.getElementById("btn-withdraw").addEventListener('click',function(){
 const withDrawField = document.getElementById("WithDraw-field");
 const newWithdrawAmountString = withDrawField.value;
 const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+// for clear step-7 ///
+withDrawField.value = '';
+//Extra work!! for remove none////
+if(isNaN(newWithdrawAmount)){
+    alert('Please! Provide a Valid Number');
+    return;
+}
+
 // /step-3////
 const previousWithdrawElement =document.getElementById("withdwar-total");
 const previousWithDrawString = previousWithdrawElement.innerText;
 const previousWithDraw = parseFloat(previousWithDrawString);
 
-// step-4 total withdraw  ammount//
-const previousTotalWithDraw = previousWithDraw+newWithdrawAmount;
 
-previousWithdrawElement.innerText = previousTotalWithDraw;
 // step-5///
 
 const balanceElement = document.getElementById("balance-total");
@@ -21,8 +27,18 @@ const previousBalanceTotal =parseFloat(previousBalanceTotalString);
 const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
 
+if(newWithdrawAmount>= previousBalanceTotal){
+    alert('You cannot WithDraw more than your balance');
+    return;
+}
+// step-4 total withdraw  ammount//
+const previousTotalWithDraw = previousWithDraw+newWithdrawAmount;
+
+previousWithdrawElement.innerText = previousTotalWithDraw;
+
+
+// step-6///
 balanceElement.innerText =newBalanceTotal;
-// for clear ///
-withDrawField.value = ''
+
 
 })
